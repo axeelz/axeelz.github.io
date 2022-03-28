998f86134ac878bb09057ed89c2ad1d6a969e814
axeelz <83944331+axeelz@users.noreply.github.com>
2022-03-28 22:19:39 +0200

    tabs format

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -5,26 +5,26 @@ const themeColor = document.querySelector('meta[name="theme-color"]');
 const currentTheme = localStorage.getItem("theme");
 
 if (userPrefersDark && currentTheme === null) {
-    toggleDarkMode();
-    themeColor.setAttribute('content', '#000000');
+	toggleDarkMode();
+	themeColor.setAttribute('content', '#000000');
 }
 
 if (userPrefersDark && currentTheme == "dark") {
-    themeColor.setAttribute('content', '#000000');
+	themeColor.setAttribute('content', '#000000');
 }
 
 if (currentTheme == "dark") {
-    document.body.classList.add("dark-theme");
+	document.body.classList.add("dark-theme");
 }
 
 function toggleDarkMode() {
-    document.body.classList.toggle("dark-theme");
-    let theme = "light";
-    let color = "#FAF8FF";
-    if (document.body.classList.contains("dark-theme")) {
-        theme = "dark";
-        color = "#000000";
-    }
-    localStorage.setItem("theme", theme);
-    themeColor.setAttribute('content', color);
+	document.body.classList.toggle("dark-theme");
+	let theme = "light";
+	let color = "#FAF8FF";
+	if (document.body.classList.contains("dark-theme")) {
+		theme = "dark";
+		color = "#000000";
+	}
+	localStorage.setItem("theme", theme);
+	themeColor.setAttribute('content', color);
 }
