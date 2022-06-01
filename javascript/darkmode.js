15d20d18fdbab68d7f50d51710832e3e65aa3a59
axeelz <83944331+axeelz@users.noreply.github.com>
2022-06-01 03:39:01 +0200

    lint + nav flex

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,31 +1,34 @@
-const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const userPrefersLight =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches;
 const themeColor = document.querySelector('meta[name="theme-color"]');
 
 const currentTheme = localStorage.getItem("theme");
 
 if (userPrefersDark && currentTheme === null) {
-	toggleDarkMode();
-	themeColor.setAttribute('content', '#000000');
+  toggleDarkMode();
+  themeColor.setAttribute("content", "#000000");
 }
 
 if (userPrefersDark && currentTheme == "dark") {
-	themeColor.setAttribute('content', '#000000');
+  themeColor.setAttribute("content", "#000000");
 }
 
 if (currentTheme == "dark") {
-	document.body.classList.add("dark-theme");
+  document.body.classList.add("dark-theme");
 }
 
 function toggleDarkMode() {
-	document.body.classList.toggle("dark-theme");
-	let theme = "light";
-	let color = "#FAF8FF";
-	if (document.body.classList.contains("dark-theme")) {
-		theme = "dark";
-		color = "#000000";
-	}
-	localStorage.setItem("theme", theme);
-	themeColor.setAttribute('content', color);
+  document.body.classList.toggle("dark-theme");
+  let theme = "light";
+  let color = "#FAF8FF";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+    color = "#000000";
+  }
+  localStorage.setItem("theme", theme);
+  themeColor.setAttribute("content", color);
 }
-
